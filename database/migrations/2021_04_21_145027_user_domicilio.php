0261f7a39174bfd72f3d50c37b9ebe34b347b6ec
Xavi Morales <jamoco_1990@hotmail.com>
2021-04-21 09:55:03 -0500

    inico de proyecto de prueba

---
+++ database/migrations/2021_04_21_145027_user_domicilio.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UserDomicilio extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_domicilio', function (Blueprint $table) {
+            $table->id('user_id');
+            $table->string('domicilio');
+            $table->string('numero_exterior');
+            $table->string('colonia');
+            $table->string('cp');
+            $table->string('ciudad');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
